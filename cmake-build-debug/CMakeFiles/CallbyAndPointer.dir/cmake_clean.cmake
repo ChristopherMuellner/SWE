e811d683c0f2e1dc2ac97119a29e1d8b25210588
vujh <officialvujh@gmail.com>
2024-10-24 20:53:00 +0200

    unresolved changes

---
+++ cmake-build-debug/CMakeFiles/CallbyAndPointer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CallbyAndPointer.dir/main.cpp.obj"
+  "CMakeFiles/CallbyAndPointer.dir/main.cpp.obj.d"
+  "CallbyAndPointer.exe"
+  "CallbyAndPointer.exe.manifest"
+  "CallbyAndPointer.pdb"
+  "libCallbyAndPointer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CallbyAndPointer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CallbyAndPointer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CallbyAndPointer.
+# This may be replaced when dependencies are built.
