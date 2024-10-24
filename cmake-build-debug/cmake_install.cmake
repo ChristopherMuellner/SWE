b3fc252acebaf0e5a546b90867dd17c3309cc9c5
vujh <officialvujh@gmail.com>
2024-10-24 21:16:37 +0200

    unresolved changes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/chris/OneDrive - HTL Hollabrunn/Projects/Software/HTL
+# Install script for directory: C:/Users/chris/Projects/Software/FilesC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/chris/OneDrive - HTL Hollabrunn/Projects/Software/HTL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/chris/Projects/Software/FilesC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
