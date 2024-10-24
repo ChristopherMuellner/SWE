b709194c8b86aace85cdbb084b0bd0f8222e0a36
vujh <officialvujh@gmail.com>
2024-10-24 21:03:02 +0200

    unresolved changes

---
+++ cmake-build-debug/CMakeFiles/TimeSpan.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimeSpan.dir/Source_Files/TimeSpan.cpp.obj"
+  "CMakeFiles/TimeSpan.dir/Source_Files/TimeSpan.cpp.obj.d"
+  "CMakeFiles/TimeSpan.dir/Source_Files/main.cpp.obj"
+  "CMakeFiles/TimeSpan.dir/Source_Files/main.cpp.obj.d"
+  "TimeSpan.exe"
+  "TimeSpan.exe.manifest"
+  "TimeSpan.pdb"
+  "libTimeSpan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimeSpan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TimeSpan.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TimeSpan.
+# This may be replaced when dependencies are built.
