24371618c6122bc87fd5109ead07b37a308ff563
vujh <officialvujh@gmail.com>
2024-10-24 21:01:32 +0200

    unresolved changes

---
+++ cmake-build-debug/CMakeFiles/stringFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stringFunctions.dir/main.c.obj"
+  "CMakeFiles/stringFunctions.dir/main.c.obj.d"
+  "libstringFunctions.dll.a"
+  "stringFunctions.exe"
+  "stringFunctions.exe.manifest"
+  "stringFunctions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stringFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
